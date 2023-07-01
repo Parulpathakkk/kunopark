c23853c8d1388c0feaa9657e697182266553ced4
Parulpathakkk <parul@artworksit.com>
2023-07-01 14:08:36 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+     darkgreen: '#22492C',
+     yellow : "#FFDB61",
+     textgray:"#737373",
+     lightgreen:"#22492C",
+     whitetext:'#FCFCFC',
+     textblack:"#0A0A0A"
+  
+    },
+    fontFamily: {
+     inter : 'Inter, sans-serif',
+     mont:'Montserrat, sans-serif'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
